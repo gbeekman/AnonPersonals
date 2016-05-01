d3a4fa1d68b4e6fa5e6e64a32af5cc8670da7546
Graham <grahambeekman@gmail.com>
2016-05-01 12:53:00 -0400

    Change personal create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 AnonPersonals::Application.routes.draw do
   root to: "sessions#new"
 
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do
+    resources :personals, only: :create
+  end
+
   resource :session, only: [:new, :create, :destroy]
-  resources :personals
+  resources :personals, except: :create
 
 end

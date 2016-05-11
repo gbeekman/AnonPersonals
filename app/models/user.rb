242943d72f7f2dc0aa720525c27a72ced72c5104
Graham <grahambeekman@gmail.com>
2016-05-11 16:31:57 -0400

    Create migrations and models for chat

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,13 @@ class User < ActiveRecord::Base
   primary_key: :id
   )
 
+  has_many(
+  :conversations,
+  class_name: "Conversation",
+  foreign_key: :recipient_id,
+  primary_key: :id
+  )
+
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user && user.valid_password?(password)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506201830) do
+ActiveRecord::Schema.define(version: 20160511200223) do
+
+  create_table "chats", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "chats", ["conversation_id"], name: "index_chats_on_conversation_id"
+  add_index "chats", ["user_id"], name: "index_chats_on_user_id"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "conversations", ["recipient_id"], name: "index_conversations_on_recipient_id"
+  add_index "conversations", ["sender_id"], name: "index_conversations_on_sender_id"
 
   create_table "messages", force: :cascade do |t|
     t.string   "body",         null: false

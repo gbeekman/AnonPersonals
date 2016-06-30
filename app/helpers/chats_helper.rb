8cdce4d29be6b0e4bf9c064caeb389dd1edb28a7
Graham <grahambeekman@gmail.com>
2016-06-30 14:50:44 -0400

    write chat helpers

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -1,2 +1,9 @@
 module ChatsHelper
+  def self_or_other(chat)
+    chat.user == current_user ? "self" : "other"
+  end
+
+  def chat_interlocutor(chat)
+    chat.user == chat.conversation.sender ? chat.conversation.sender : chat.conversation.recipient
+  end
 end
